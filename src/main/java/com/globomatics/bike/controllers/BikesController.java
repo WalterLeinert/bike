1c18ac05743e799db7ede94a7d556c801bb9940a
Walter Leinert <walter.leinert@hp.com>
2018-07-03 12:50:49 +0200

    added persistence

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -3,6 +3,7 @@ package com.globomatics.bike.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,26 +14,30 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.globomatics.bike.models.Bike;
+import com.globomatics.bike.repositories.BikeRepository;
 
 @RestController()
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
 	
+	@Autowired
+	private BikeRepository repository;
+	
 	@GetMapping()
 	public List<Bike> list() {
-		return new ArrayList<>();
+		return repository.findAll();
 	}
 	
 	
 	@PostMapping()
 	@ResponseStatus(HttpStatus.OK)
 	public void create(@RequestBody Bike bike) {
-		
+		repository.save(bike);
 	}
 
 	
 	@GetMapping("/{id}")
 	public Bike get(@PathVariable("id") long id) {
-		return new Bike();
+		return repository.getOne(id);
 	}
 }
